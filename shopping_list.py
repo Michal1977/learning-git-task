90ade7c389b42b22e993ba497e4d81ba65e8d376
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-02-23 23:00:21 +0100

    Add store

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,5 @@ zakupy = (lista_zakupów["piekarnia"]+lista_zakupów["warzywniak"])
 for sklep, produkt in lista_zakupów.items():
     print("Idę do",sklep.capitalize(),",","kupuję tu następujące rzeczy:",produkt)
 print("W sumie kupuję", (len(zakupy)),"produktów")
+lista_zakupów["kiosk"]=["gazeta", "bilet"]
+print(lista_zakupów)
