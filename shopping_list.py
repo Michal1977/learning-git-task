f51ce4bdef1d4b3fb7d5808788991c793a507d36
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-02-23 22:42:29 +0100

    Capitalize shop name

--- shopping_list.py
+++ shopping_list.py
@@ -1,2 +1,4 @@
 lista_zakupów={"piekarnia":["chleb", "pączek", "bułki"], "warzywniak":["marchew","seler","rukola"]}
 zakupy = (lista_zakupów["piekarnia"]+lista_zakupów["warzywniak"])
+for sklep, produkt in lista_zakupów.items():
+    print("Idę do",sklep.capitalize(),",","kupuję tu następujące rzeczy:",produkt)
