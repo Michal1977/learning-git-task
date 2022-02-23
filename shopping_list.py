aceb4fe53a44723a7d632e32bba2a0df4027d65a
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-02-23 22:43:27 +0100

    Add writing

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,4 @@ lista_zakupów={"piekarnia":["chleb", "pączek", "bułki"], "warzywniak":["march
 zakupy = (lista_zakupów["piekarnia"]+lista_zakupów["warzywniak"])
 for sklep, produkt in lista_zakupów.items():
     print("Idę do",sklep.capitalize(),",","kupuję tu następujące rzeczy:",produkt)
+print("W sumie kupuję", (len(zakupy)),"produktów")
