5cdea3edee6d9fba0ab4e9c07d908d5a8fda3d24
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-02-25 21:46:42 +0100

    Removed pączek

--- shopping_list.py
+++ shopping_list.py
@@ -5,3 +5,5 @@ for sklep, produkt in lista_zakupów.items():
 print("W sumie kupuję", (len(zakupy)),"produktów")
 lista_zakupów["kiosk"]=["gazeta", "bilet"]
 print(lista_zakupów)
+lista_zakupów["piekarnia"]=["chleb", "bułki"]
+print(lista_zakupów)
